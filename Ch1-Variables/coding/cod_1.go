383a2ab99ca9210da787a55bdd1961d252ae5785
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-04-27 09:27:38 +0530

    variables: coding: solve 2 coding questions

---
+++ Ch1-Variables/coding/cod_1.go
@@ -0,0 +1,19 @@
+/*
+Coding Q1:
+Declare two variables:
+
+One int named students = 45
+
+One string named school = "ABC High School"
+Print them both together.
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+	var students int = 45
+	var school string = "ABC High School"
+	fmt.Println(students,school)
+}
---
+++ Ch1-Variables/coding/cod_2.go
@@ -0,0 +1,14 @@
+/*
+Coding Q2:
+Create a constant named SpeedOfLight and set it to 299792458.
+Then print "Speed of light is [value] m/s".
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	const SpeedOfLight = 299792458
+	fmt.Printf("Speed of Light is %d m/s\n", SpeedOfLight)
+}
